27a5c4408e9b633d32176faf8b760a4358665ded
Laurent David <lmedavid@googlemail.com>
2015-04-03 19:27:12 +0200

    Split the JS from the HTML

--- js-input-helper.js
+++ js-input-helper.js
@@ -16,13 +16,16 @@ program
 		process.exit(1);
 	}
 	mkdir('-p', project_name);
-	cp('-R',require('path').dirname(require.main.filename)+'/project-template/*',project_name)
-	cd(project_name)
-	mv('project-template.html',project_name+'.html')
-	mv('project-template.xml',project_name+'.xml')
-	sed('-i','PROJECT-NAME',project_name,project_name+'.html')
-	sed('-i','PROJECT-NAME',project_name,project_name+'.xml')
-	sed('-i','PROJECT-NAME',project_name,'README.md')
+	cp('-R',require('path').dirname(require.main.filename)+'/project-template/*',project_name);
+	cd(project_name);
+	mv('project-template.html',project_name+'.html');
+	mv('project-template.js',project_name+'.js');
+	mv('project-template.xml',project_name+'.xml');
+	sed('-i',/@PROJECT-NAME@/g,project_name,project_name+'.html');
+	sed('-i',/project-template/g,project_name,project_name+'.html');
+	sed('-i',/@PROJECT-NAME@/g,project_name,project_name+'.xml');
+	sed('-i',/@PROJECT-NAME@/g,project_name,'README.md');
+
     });
 
 program.parse(process.argv);
